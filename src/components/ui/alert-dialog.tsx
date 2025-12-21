15b7b71fdfb4f6f549a5499e62461edc1dda3f1e
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-12-21 15:53:44 +0530

    const data

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -7,7 +7,6 @@ import { buttonVariants } from "@/components/ui/button";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
-
 const AlertDialogPortal = AlertDialogPrimitive.Portal;
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
